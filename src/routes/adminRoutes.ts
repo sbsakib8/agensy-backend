dcb44131337f9b4b9b246a879f2acf7a4c391cb6
riyad899 <raditkhanyt@gmail.com>
2026-01-28 02:15:23 +0600

    Remove all code from main branch

--- src/routes/adminRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { createUser } from '../controllers/adminController'
-
-const router = Router()
-
-router.post('/create-user', createUser)
-
-export default router
